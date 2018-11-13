8a91279e9af18d54fad13293ce4f71da012ecaab
Thiago de Sousa Garcia <42843882+ThiagoSousaGarcia@users.noreply.github.com>
2018-11-12 23:30:53 -0300

    Ajuste no método descontar()

--- ocp/ContaPoupanca.java
+++ ocp/ContaPoupanca.java
@@ -9,6 +9,8 @@ public class ContaPoupanca extends Debitar{
 
     /*Vamos supor que na conta poupança, ao invés de descontar seja acrescentado 10% do saldo mensalmente*/ 
     public double descontar(){
-        return this.saldo + 0.1*this.saldo;
+        this.saldo = this.saldo + 0.1*this.saldo; 
+        
+        return this.saldo;
     }
 }
